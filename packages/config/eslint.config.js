cc0097d2dd60a811bbbd97670e7f604aee371438
opeblow <opeblow2021@gmail.com>
2026-09-11 01:34:13 -0700

    chore: bootstrap haven monorepo tooling

---
+++ packages/config/eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+  ignorePatterns: ["node_modules/", "dist/", ".next/"],
+};
